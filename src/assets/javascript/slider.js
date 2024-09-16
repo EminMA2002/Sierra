fd6e633e6096976237b7e3b907000505edec7bd8
Emin <e.mmmdv02@gmail.com>
2024-09-16 15:37:36 +0400

    Sierra v1.0

---
+++ src/assets/javascript/slider.js
@@ -0,0 +1,15 @@
+let carousel = document.querySelector(".carousel");
+let btn1 = document.querySelector(".carouselbutton");
+let btn2 = document.querySelector(".carouselbutton1");
+let btn3 = document.querySelector(".carouselbutton2");
+let btn4 = document.querySelector(".carouselbutton3");
+let btn5 = document.querySelector(".carouselbutton4");
+let index = 0;
+
+function circle(){
+    console.log("salam");
+    
+}
+
+
+btn1.addEventListener("click", circle);
